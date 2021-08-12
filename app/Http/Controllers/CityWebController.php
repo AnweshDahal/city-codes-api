8212f07bb103cf8fbe8b5abb303d9d22021f848c
AnweshDahal <anweshd2000@gmail.com>
2021-08-12 16:34:07 +0545

    Adds controller for city web view

--- app/Http/Controllers/CityWebController.php
+++ app/Http/Controllers/CityWebController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\City;
 
 class CityWebController extends Controller
 {
     //
+    public function index(){
+        $cities = City::all();
+        return view('city.index', compact('cities'));
+    }
+
+    public function create(){
+        return view('city.create');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name'=>'required|unique:cities',
+            'code'=>'required|unique:cities'
+        ]);
+
+        City::create($request->all());
+
+        return redirect('/city/create');
+    }
 }
