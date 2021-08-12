69303c69819140c26031b2c598fcffdbdf272f25
AnweshDahal <anweshd2000@gmail.com>
2021-08-12 16:34:39 +0545

    Adds create city form

--- resources/views/city/create.blade.php
+++ resources/views/city/create.blade.php
@@ -1,17 +1,27 @@
 @extends('layout.layout')
 @section('contents')
-    <form action="" method="post">
+    <form action="{{ route('store') }}" method="post" enctype="multipart/form-data">
         @csrf
-        <div class="mb-3">
-            <label>City Name</label>
-            <input type="text" name="name" id="cityName" class="form-control">
+        <div class="row">
+            <div class="mb-3 col">
+                <label>City Name</label>
+                <input type="text" name="name" id="cityName" class="form-control">
+                <div id="passwordHelpBlock" class="form-text">
+                    {{ $errors->first('name') }}
+                </div>
+            </div>
+            <div class="mb-3 col">
+                <label>City Code</label>
+                <input type="text" name="code" id="cityName" class="form-control" maxlength="3">
+                <div id="passwordHelpBlock" class="form-text">
+                    {{ $errors->first('code') }}
+                </div>
+            </div>
         </div>
-        <div class="mb-3">
-            <label>City Code</label>
-            <input type="text" name="code" id="cityName" class="form-control" maxlength="3">
-        </div>
-        <div class="mb-3">
-            <button type="submit" class="btn btn-primary">Add</button>
+        <div class="row">
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Add</button>
+            </div>
         </div>
     </form>
 @endsection
